12f989db02da34374af565b68f9c046619aee245
Ian Rees <ian@ianrees.net>
2026-04-04 04:16:01 -0700

    Export eslint/stylistic configs

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,6 @@
 // @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
-
-const stylisticConfig = {
-  flat: true,
-  indent: 2,
-  quotes: 'single',
-  semi: false,
-}
+import { stylisticConfig, ignoreFiles, eslintConfig } from './src/eslint'
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -23,32 +17,5 @@ export default createConfigForNuxt({
     ],
   },
 })
-  .prepend({
-    ignores: [
-      '.nuxt/**',
-      '.output/**',
-      '**/.nuxt',
-      'dist/**',
-      'node_modules/**',
-    ],
-  })
-  .append({
-    rules: {
-      'no-console': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/unified-signatures': 'off',
-      'vue/multi-word-component-names': 'off',
-      'vue/max-attributes-per-line': ['error', {
-        singleline: { max: 10 },
-        multiline: { max: 1 },
-      }],
-      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-      '@stylistic/space-before-function-paren': ['error', {
-        anonymous: 'always',
-        named: 'always',
-        asyncArrow: 'always',
-      }],
-      '@stylistic/comma-dangle': 'off',
-      '@stylistic/max-statements-per-line': ['error', { max: 3 }],
-    },
-  })
+  .prepend(ignoreFiles)
+  .append(eslintConfig)
